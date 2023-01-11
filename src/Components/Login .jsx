0d997d8c4d5b7b6111115ddd2b516f1f8c199d0c
Gerald-ux-ux <11geraldmoney@gmail.com>
2023-01-11 12:23:04 +0300

    login page

--- src/Components/Login .jsx
+++ src/Components/Login .jsx
@@ -1,37 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Img from "../Components/img/undraw_Bus_stop_re_h8ej.png";
+
 
 const Login = () => {
   return (
     <div>
+      <div className="img">
+        <img className="img-tag" src={Img} alt={Img} />
+      </div>
       <div>
         <h3 className="Textaa">Login</h3>
       </div>
-      <form>
-        <div class="form-group">
-          <label >Email address</label>
-          <input
-            type="email"
-            class="form-control"
-            placeholder="Enter email"
-          />
-          <small className="SmallText">
-            Enter your phone number
-          </small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-          />
-          <small className="SmallText">Enter your password</small>
-        </div>
-        <button type="submit" class="btn btn-light">
-          Submit
-        </button>
-      </form>
+      <div className="Parent1">
+        <form>
+          <div class="form-group">
+            <label>Phone number</label>
+            <input
+              type="email"
+              class="form-control"
+              placeholder="Enter phone number"
+            />
+            <small className="SmallText">Enter your phone number</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+            <small className="SmallText">Enter your password</small>
+            <p>
+              Don't have an account?{" "}
+              <Link className="singuptxt" to="/signup">
+                Sign up here
+              </Link>
+            </p>
+          </div>
+          <button type="submit" class="btn btn-success">
+            Log in
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
