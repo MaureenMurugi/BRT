9392fed1d11e528a811af4c7a5f111b3269e9be3
Martin Kamau <martinkimu09@gmail.com>
2023-01-11 12:21:05 +0300

    Updated AboutUs.js

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -4,6 +4,20 @@ function AboutUs() {
   return (
     <div>
         <h1>About Bus Transit Service</h1>
+        <div>
+            <h2>What We Do</h2>
+            <ul>
+                <li>Pick you at your door step</li>
+                <li>Give you fair bus rates</li>
+                <li>Take you where you want to go</li>
+                <li>Take you there fast</li>
+            </ul>
+        </div>
+        <h2>Who We Work With</h2>
+        <h2>Our Mission</h2>
+        <li>To serve you how we know best.</li>
+        <h2>Our Vision</h2>
+        <li>Provide our services to all towns by 2025.</li>
     </div>
   )
 }
