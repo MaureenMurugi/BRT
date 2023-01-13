5732980968ef7115826a6ec31cdb610a562bd548
Martin Kamau <martinkimu09@gmail.com>
2023-01-13 08:06:48 +0300

    Updated AboutUs.js

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -3,27 +3,25 @@ import React from 'react'
 function AboutUs() {
   return (
     <div>
-        <h1>About Bus Transit Service</h1>
-        <div>
-            <h2>What We Do</h2>
-            <ul>
-                <li>Pick you at your door step</li>
-                <li>Give you fair bus rates</li>
-                <li>Take you where you want to go</li>
-                <li>Take you there fast</li>
-            </ul>
+        <div className='about'>
+            <h1 className='header'>About Bus Transit Service</h1>
+            <div>
+                <h2 className='header'>What We Do</h2>
+                <ul>
+                    <li>Pick you at your door step</li>
+                    <li>Give you fair bus rates</li>
+                    <li>Take you where you want to go</li>
+                    <li>Take you there fast</li>
+                </ul>
+            </div>
+            <h2 className='header'>Who We Work With</h2>
+            <h2 className='header'>Our Mission</h2>
+            <p>To serve you how we know best.</p>
+            <h2 className='header'>Our Vision</h2>
+            <p>Provide our services to all towns by 2025.</p>
         </div>
-        <h2>Who We Work With</h2>
-        <h2>Our Mission</h2>
-        <ul>
-            <li>To serve you how we know best.</li>
-        </ul>
-        <h2>Our Vision</h2>
-        <ul>
-            <li>Provide our services to all towns by 2025.</li>
-        </ul>
     </div>
   )
 }
 
-export default AboutUs
+export default AboutUs;
