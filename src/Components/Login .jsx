d50eeb6344262507dd4429facc05c4f1beff005b
Jackson-Kahono <jackson.kahono@student.moringaschool.com>
2023-01-14 15:24:29 +0300

    Add style for error div, can be improved

--- src/Components/Login .jsx
+++ src/Components/Login .jsx
@@ -7,11 +7,32 @@ import Img from "../Components/img/undraw_Bus_stop_re_h8ej.png";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [error, setError] = useState({phonenumber:"" , password:""});
+  const [error, setError] = useState({
+    phonenumber: "",
+    password: "",
+    status: "",
+  });
+
+  const redDiv = {
+    width: "300px",
+    height: "37px",
+    border: "1px solid red",
+    display: "flex",
+    padding: "5px 0px",
+    alignItems: "center",
+    flexDirection: "column",
+    borderRadius: "20px",
+  };
+
+  const noDiv = {
+    display: "none",
+    width: "300px",
+    border: "1px solid red",
+  };
 
   function validatePhonenumber(phonenumber) {
-    //test for 10 digits or 12 digits starting with 254
-    const re = /^(\+254|0)[7][0-9]{8}$/;
+    //test for 10 digits or 12 digits starting with 254 followed by 9 digits
+    const re = /^(254|0)?(7|1)\d{8}$/;
     return re.test(phonenumber);
   }
 
@@ -36,6 +57,8 @@ const Login = () => {
       return;
     }
 
+    setError({ ...error, phonenumber: "", password: "", status: "" });
+
     //post to the api
     fetch("/login", {
       method: "POST",
@@ -48,7 +71,8 @@ const Login = () => {
         if (res.ok) {
           return res.json();
         } else {
-          setError({ error: "Something went wrong" });
+          setError({ ...error, status: "Something went wrong" });
+          return null;
         }
       })
       .then((data) => {
@@ -68,9 +92,22 @@ const Login = () => {
         <h3 className="Textaa">Login</h3>
       </div>
       <div className="auth-errors-wrapper">
-        <div className="auth-errors" style={error.phonenumber === "" || error.password === "" ? {"display":"none"} : {"display":"block"}}>
-          <p>{error.phonenumber}</p>
-          <p>{error.password}</p>
+        <div
+          className="auth-errors"
+          style={
+            error.phonenumber === "" &&
+            error.password === "" &&
+            error.status === ""
+              ? noDiv
+              : redDiv
+          }
+        >
+          <p>
+            {error.phonenumber === "" && error.password === ""
+              ? ""
+              : "Invalid phone number or password"}
+            {error.status !== "" ? "Something went wrong" : ""}
+          </p>
         </div>
       </div>
 
@@ -88,6 +125,11 @@ const Login = () => {
               required
               autoFocus="yes"
             />
+            <div className="col-auto">
+              <span id="passwordHelpInline" className="SmallText">
+               (2547xxxxxxxx) or (07xxxxxxxx)
+              </span>
+            </div>
             {/* <small className="SmallText">Enter your phone number</small> */}
           </div>
           <div className="form-group">
