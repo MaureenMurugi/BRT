81b45986bba5c6ab23317cb0c86db4db57d30605
Jackson-Kahono <jackson.kahono@student.moringaschool.com>
2023-01-18 13:15:54 +0300

    testing depl

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 //read apiurl from .env file
-// const API_URL = process.env.REACT_APP_API_URL;
-const API_URL = "http://localhost:3000";
+const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = "http://localhost:3000";
 
 export {API_URL};
