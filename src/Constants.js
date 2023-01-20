70cb9d60a0de7f4994bd274f395c4c892cca4d7e
Jackson-Kahono <jackson.kahono@student.moringaschool.com>
2023-01-20 09:05:11 +0300

    Changing api url

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 //read apiurl from .env file
 // const API_URL = process.env.REACT_APP_API_URL;
-const API_URL = "http://localhost:3000";
+const API_URL = "https://modern-burst-production.up.railway.app";
 
 export {API_URL};
